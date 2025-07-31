72642dde93f4440aee05fff459bffde46b8ba46d
Yash Macbook <yashhhgajera@gmail.com>
2025-07-31 12:38:28 -0400

    init project

---
+++ src/test/java/com/newsanalyzer/api/NewsSentimentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsanalyzer.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsSentimentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

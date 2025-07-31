5577b46dcd884eda5705090dff7b2f23ba25ca62
Yash Macbook <yashhhgajera@gmail.com>
2025-07-31 17:27:25 -0400

    add dotenv support and update application properties for NewsAPI configuration

--- src/main/java/com/newsanalyzer/api/NewsSentimentApiApplication.java
+++ src/main/java/com/newsanalyzer/api/NewsSentimentApiApplication.java
@@ -3,10 +3,19 @@ package com.newsanalyzer.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class NewsSentimentApiApplication {
 
 	public static void main(String[] args) {
+
+		// Load .env file
+        Dotenv dotenv = Dotenv.load();
+
+        // Set as system property so Spring can read it using ${NEWSAPI_KEY}
+        System.setProperty("NEWSAPI_KEY", dotenv.get("NEWSAPI_KEY"));
+		
 		SpringApplication.run(NewsSentimentApiApplication.class, args);
 	}
 
